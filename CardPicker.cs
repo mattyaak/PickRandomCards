5b67c32cad49a766b24a0bd1d9bbbaa24e73d11e
Mattyasovszky <MattyaAk@u131.nato.int>
2021-01-06 15:08:21 +0100

    Add project files.

---
+++ CardPicker.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PickRandomCards
+{
+    class CardPicker
+    {
+        static Random random = new Random();
+        public static string[] PickSomeCards (int numberOfCards)
+        {
+            string[] pickedCards = new string[numberOfCards];
+            for (int i = 0; i < numberOfCards; i++)
+            {
+                pickedCards = RandomValue() + " of " + RandomSuit();
+            }
+            return pickedCards;
+        }
+
+        private static string RandomSuit()
+        {
+            int value = random.Next(1, 5);
+            if (value == 1)
+            {
+                return 
+            }
+        }
+
+        private static string RandomValue()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
